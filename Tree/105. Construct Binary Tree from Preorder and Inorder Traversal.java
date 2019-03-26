ad151ac26a01d3975466b808c4cd7ee2e1753032
drake <zzhmessi@163.com>
2019-03-26 11:53:59 -0700

    Create 105. Construct Binary Tree from Preorder and Inorder Traversal.java

---
+++ Tree/105. Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return helper(0, 0, inorder.length - 1, preorder, inorder);
+    }
+    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+        if (preStart >= preorder.length || inStart > inEnd) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int index = 0;
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == preorder[preStart]) {
+                index = i; //找到root在inorder里的位置
+            }
+        }
+        root.left = helper(preStart + 1, inStart, index - 1, preorder, inorder);
+        root.right = helper(preStart + index - inStart + 1, index + 1, inEnd, preorder, inorder); //这里要理解透彻了
+        return root;
+    }
+}
