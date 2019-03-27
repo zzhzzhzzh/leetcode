9cb35ec20c8c99dfb3d1c7a2598b7d9f7d8ce282
drake <zzhmessi@163.com>
2019-03-27 11:43:29 -0700

    Create 560. Subarray Sum Equals K.java

---
+++ Array,List,Interval/560. Subarray Sum Equals K.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int res = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            int target = sum - k;
+            if (map.containsKey(target)) {
+                res += map.get(target);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            
+        }
+        return res;
+    }
+}
