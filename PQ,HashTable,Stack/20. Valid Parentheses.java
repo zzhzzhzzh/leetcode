877f2014b1223b393c562cf017e9973f1a154393
drake <zzhmessi@163.com>
2019-03-27 22:11:15 -0700

    Create 20. Valid Parentheses.java

---
+++ PQ,HashTable,Stack/20. Valid Parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.peek() != '{') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
