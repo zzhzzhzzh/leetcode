cc95edc2b9f5a11422442c11b40d54590dd3e3ee
drake <zzhmessi@163.com>
2019-03-28 09:08:20 -0700

    Create 155. Min Stack.java

---
+++ PQ,HashTable,Stack/155. Min Stack.java
@@ -0,0 +1,43 @@
+class MinStack {
+
+    /** initialize your data structure here. */
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+    public MinStack() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+    
+    public void push(int x) {
+        stack1.push(x);
+        if (stack2.isEmpty() ) {
+            stack2.push(x);
+        } else if (x < stack2.peek()) {
+            stack2.push(x);
+        } else {
+            stack2.push(stack2.peek());
+        }
+    }
+    
+    public void pop() {
+        stack1.pop();
+        stack2.pop();
+    }
+    
+    public int top() {
+        return stack1.peek();
+    }
+    
+    public int getMin() {
+        return stack2.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
