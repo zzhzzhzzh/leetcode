2f7a926e61607390eb4f810f4a00312b41e9947f
drake <zzhmessi@163.com>
2019-03-29 15:52:16 -0700

    Create 224. Basic Calculator.java

---
+++ PQ,HashTable,Stack/224. Basic Calculator.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int calculate(String s) {
+        if (s == null || s.length() == 0) return 0;
+        Stack<Integer> stack = new Stack<>();
+        int number = 0;
+        int sign = 1;
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                number = number * 10 + (int)(c - '0');
+            } else if (c == '+') {
+                result += sign * number;
+                number = 0;
+                sign = 1;
+            } else if (c == '-') {
+                result += sign * number;
+                number = 0;
+                sign = -1;
+            } else if (c == '(') {
+                stack.push(result);
+                stack.push(sign);
+                sign = 1;
+                result = 0;
+            } else if (c == ')') {
+                result += sign * number;
+                number = 0;
+                result *= stack.pop();
+                result += stack.pop();
+            }
+        }
+        if (number != 0) {
+            result += sign * number;
+        }
+        return result;
+    }
+}
