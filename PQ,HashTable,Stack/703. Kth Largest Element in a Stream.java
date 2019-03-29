fad921a4ed3e8baedd71455d8f113a57bccc807f
drake <zzhmessi@163.com>
2019-03-29 10:48:32 -0700

    Create 703. Kth Largest Element in a Stream.java

---
+++ PQ,HashTable,Stack/703. Kth Largest Element in a Stream.java
@@ -0,0 +1,26 @@
+class KthLargest {
+
+    int k;
+    PriorityQueue<Integer> pq;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>(); // 最小堆
+        for (int num: nums) {
+            add(num);
+        }
+    }
+    
+    public int add(int val) {
+        pq.offer(val);
+        if (pq.size() > k) {
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
