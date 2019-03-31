792df972006acda6a5b09579737e357e03da100c
drake <zzhmessi@163.com>
2019-03-31 16:36:32 -0700

    Create 33. Search in Rotated Sorted Array.java

---
+++ binary search, two pointers/33. Search in Rotated Sorted Array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0, right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[nums.length - 1]) {
+                if (target >= nums[0] && target <= nums[mid]) {
+                    right = mid;
+                } else {
+                    left = mid;
+                }
+            } else {
+                if (target >= nums[mid] && target <= nums[nums.length - 1]) {
+                    left = mid;
+                } else {
+                    right = mid;
+                }
+            }
+        }
+        if (nums[left] == target) {
+            return left;
+        }
+        if (nums[right] == target) {
+            return right;
+        }
+        return -1;
+    }
+    
+}
