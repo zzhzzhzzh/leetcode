2554c1ade04de0315a2f0e104c28cd796ddcba9b
drake <zzhmessi@163.com>
2019-03-31 20:52:03 -0700

    Update all path between two nodes.java

--- dfs,bfs,dp/all path between two nodes.java
+++ dfs,bfs,dp/all path between two nodes.java
@@ -1,3 +1,29 @@
+//邻接表中两个点之间所有的路径
+public class Solution {
+    /**
+     * @param graph: a 2D array
+     * @return: all possible paths from node 0 to node N-1
+     */
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        path.add(0);
+        dfs(graph, 0, path, res);
+        return res;
+    }
+    public void dfs(int[][] graph, int node, List<Integer> path, List<List<Integer>> res) {
+        if (node == graph.length - 1) {
+            res.add(new ArrayList<Integer>(path));
+            return;
+        }
+        for (int next: graph[node]) {
+            path.add(next);
+            dfs(graph, next, path, res);
+            path.remove(path.size() - 1);
+        }
+    }
+}
+//矩阵从左上角到右下角
 public List<String> printPath(int[][] m, int n)
     {         
          List<String> res = new ArrayList<>();
