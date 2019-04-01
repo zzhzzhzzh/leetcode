21ec4d69d0dab9654e4dcf9e7fb5e7f7699bf40d
drake <zzhmessi@163.com>
2019-04-01 16:42:25 -0700

    Update 213. House Robber II.java

--- dp/213. House Robber II.java
+++ dp/213. House Robber II.java
@@ -27,3 +27,33 @@ class Solution {
     return dp[j];
 }
 }
+
+//滚动数组优化
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(nums == null || n == 0) {
+            return 0;
+        }
+        if(n == 1) {
+            return nums[0];   
+        }
+        return Math.max(robber(nums, 0, nums.length - 2), robber(nums, 1, nums.length - 1));
+    }
+    public int robber(int[] nums, int st, int ed) {
+        int[] res = new int[2];
+        if(st == ed) {
+            return nums[st];
+        }
+        if(st + 1 == ed) {
+            return Math.max(nums[st], nums[ed]);
+        }
+        res[st % 2] = nums[st];
+        res[(st+1) % 2] = Math.max(nums[st], nums[st + 1]);
+        
+        for(int i = st + 2; i <= ed; i++) {
+            res[i % 2] = Math.max(res[(i-1) % 2], res[(i-2)%2] + nums[i]);
+        }
+        return res[ed % 2];
+    }
+}
